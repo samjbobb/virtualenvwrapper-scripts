b337ed21897a8684ff04626e6fdd1fb6ae5e831b
samjbobb <samjbobb@gmail.com>
2015-05-21 12:24:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Not the nicest install script in the world 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "Installing virtualenvwrapper-scripts. From ["$DIR"] To ["$VIRTUALENVWRAPPER_HOOK_DIR"]"
+
 # TODO: test contents of these files before deleting
 # Maybe check if all lines start with # ?
 rm -f $VIRTUALENVWRAPPER_HOOK_DIR/preactivate 
